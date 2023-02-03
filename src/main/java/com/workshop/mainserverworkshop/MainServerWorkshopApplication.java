05f9b2fc1574448fc8362ba287a0b0f0c011691d
yagil <yagilsofer@gmail.com>
2023-02-03 12:30:12 +0200

    firstCommit

---
+++ src/main/java/com/workshop/mainserverworkshop/MainServerWorkshopApplication.java
@@ -0,0 +1,13 @@
+package com.workshop.mainserverworkshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainServerWorkshopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MainServerWorkshopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/workshop/mainserverworkshop/MainServerWorkshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.workshop.mainserverworkshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MainServerWorkshopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
