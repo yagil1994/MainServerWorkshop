2dc134c33719081584eeaa88fba679a39da210cc
yagil <yagilsofer@gmail.com>
2023-02-04 14:11:50 +0200

    commmit

--- src/main/java/com/workshop/mainserverworkshop/engine/Plug.java
+++ src/main/java/com/workshop/mainserverworkshop/engine/Plug.java
@@ -2,22 +2,25 @@ package com.workshop.mainserverworkshop.engine;
 
 public class Plug {
     private Process process;
+    private boolean status;
     private String plugName;
     private int indexInList;
     private int port;
-    private DevicesMediator devicesMediator;
+    private PlugsMediator devicesMediator;
 
-    public Plug(Process i_Process, int i_port, String i_PlugName, DevicesMediator i_DevicesMediator)
+    public Plug(Process i_Process, int i_port, String i_PlugName, PlugsMediator i_DevicesMediator)
     {
         process = i_Process;
         plugName = i_PlugName;
         port = i_port;
         devicesMediator = i_DevicesMediator;
+        status = false;
     }
 
     public String off()
     {
-        return devicesMediator.sendTurOffRequestToPlug(port);
+        status = false;
+        return devicesMediator.sendTurnOffRequestToPlug(port);
     }
 
     public Process getProcess() {
@@ -30,5 +33,17 @@ public class Plug {
 
     public int getPort() {
         return port;
+    }
+
+    public void updateStatus(boolean newStatus)
+    {
+        status = newStatus;
+    }
+    public String getOnOffStatus() {
+        return status ? "on" :"off";
+    }
+
+    public boolean isOn() {
+        return status;
     }
 }
