37367d3fa46c097db35b27e41d2e2ce82bffba9c
yagil <yagilsofer@gmail.com>
2023-02-06 11:58:55 +0200

    after fixing the bugs!

--- src/main/java/com/workshop/mainserverworkshop/mediators/Plugs_Mediator.java
+++ src/main/java/com/workshop/mainserverworkshop/mediators/Plugs_Mediator.java
@@ -1,13 +1,7 @@
 package com.workshop.mainserverworkshop.mediators;
 import com.google.gson.Gson;
-import com.google.gson.JsonObject;
 import com.workshop.mainserverworkshop.engine.Plug;
 import okhttp3.*;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import java.io.IOException;
 import java.util.ArrayList;
@@ -65,21 +59,4 @@ public class Plugs_Mediator { //this mediator send http requests to the plugs(th
 
         return getResponse;
     }
-
-//    @GetMapping("/workshop/plugMediator/flipPlugModeAccordingToIndex")
-//    public ResponseEntity<String> flipPlugMode(@RequestParam String i_Plug_index_in_list){
-//        JsonObject body = new JsonObject();
-//        int plugIndex = Integer.parseInt(i_Plug_index_in_list);
-//        Plug plug = getPlugAccordingToIndex(plugIndex);
-//        boolean wasOn = plug.flipModeAndReturnPreviousMode();
-//        String translateTrueToOnOrFalseToOff = wasOn? "on" : "off";
-//        String OppositeTranslateTrueToOnOrFalseToOff = !wasOn? "on" : "off";
-//        String plugName = plug.getPlugName() + plugIndex;
-//        body.addProperty("Main serer side: ", plugName + " is going to change plug mode soon.. ");
-//        body.addProperty("Main serer side",plugName + "  mode was before: " + translateTrueToOnOrFalseToOff + " and it's " + OppositeTranslateTrueToOnOrFalseToOff);
-//        String responseFromPlug = wasOn ? plug.off() : plug.on();
-//        body.addProperty("Plug response: ", responseFromPlug);
-//
-//        return ResponseEntity.status(HttpStatus.OK).contentType(MediaType.APPLICATION_JSON).body(gson.toJson(body));
-//    }
 }
