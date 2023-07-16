195fa56e72b6c34c6f716652509d95964c4028ee
Yagil Sofer <yagilsofer@gmail.com>
2023-07-16 18:49:45 +0300

    updated the AWS server

--- src/main/java/com/workshop/mainserverworkshop/mediators/PlugsMediator.java
+++ src/main/java/com/workshop/mainserverworkshop/mediators/PlugsMediator.java
@@ -115,7 +115,6 @@ public class PlugsMediator { //this mediator sends http requests to the plugs(th
 
     private void removePlugFromAllModeLists(Plug i_Plug)
     {
-       //Plug plug = PlugsMediator.getInstance().getPlugAccordingToInternalIndex(i_PlugInternalIndex);
         signedUpPlugsForModesList.forEach(list -> list.remove(i_Plug));
     }
 
@@ -293,8 +292,8 @@ public class PlugsMediator { //this mediator sends http requests to the plugs(th
     //************************* Requests to the plug *************************/
     public String sendTurnOnOrOffRequestToPlug(int i_Port, boolean i_TurnOn) {
         String getResponse;
-        String endPoint = "http://172.31.44.173:" + i_Port + "/workshop/plug/turnOnOrOff";
-        //String endPoint = "http://localhost:" + i_Port + "/workshop/plug/turnOnOrOff";
+        String endPoint = "http://172.31.82.219:" + i_Port + "/workshop/plug/turnOnOrOff";
+        // localhost:" + i_Port + "/workshop/plug/turnOnOrOff";
         HttpUrl.Builder urlBuilder = HttpUrl.parse(endPoint).newBuilder();
         urlBuilder.addQueryParameter("TrueOrFalse", String.valueOf(i_TurnOn));
         Request request = new Request.Builder()
